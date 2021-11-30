8b9ef442100dc100918a526c6c0962ba363b475f
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-11-30 22:20:53 +0300

    Upgrade to react-router-dom 6.0.3

--- src/Overwurd.Web/ClientApp/src/App.js
+++ src/Overwurd.Web/ClientApp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { Counter } from './components/Counter';
@@ -10,8 +10,10 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/counter' element={<Counter />} />
+        </Routes>
       </Layout>
     );
   }
