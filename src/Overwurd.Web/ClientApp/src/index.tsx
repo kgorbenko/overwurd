5659081fbca7ca80be240d4affa8100fadd9244f
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-12-05 12:28:13 +0300

    Get rid of service worker

--- src/Overwurd.Web/ClientApp/src/index.tsx
+++ src/Overwurd.Web/ClientApp/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') ?? undefined;
 const rootElement = document.getElementById('root');
@@ -12,5 +11,3 @@ ReactDOM.render(
     <App />
   </BrowserRouter>,
   rootElement);
-
-registerServiceWorker();
