ba760e15b671d41e80913adc95a68edbb3cae371
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-12-11 22:15:44 +0300

    Extend Vocabulary errors

--- src/Overwurd.Model/Models/Vocabulary.cs
+++ src/Overwurd.Model/Models/Vocabulary.cs
@@ -1,31 +1,33 @@
 using System;
 using JetBrains.Annotations;
 
-namespace Overwurd.Model.Models
-{
-    public class Vocabulary : IEntity
-    {
+namespace Overwurd.Model.Models {
+    public class Vocabulary : IEntity {
         public long Id { get; [UsedImplicitly] private set; }
 
         public DateTimeOffset CreatedAt { get; [UsedImplicitly] private set; }
 
         private string name;
+
         public string Name {
             get => name;
             set {
-                if (string.IsNullOrWhiteSpace(name))
-                {
-                    throw new ArgumentException("An attempt to set Vocabulary name to an invalid value. " +
-                                                $"Vocabulary name cannot be null, empty or whitespace, but was '{value}'.");
-                }
+                string MakeMessage(string invalidValue) =>
+                    "An attempt to set Vocabulary name to an invalid value. " +
+                    $"Vocabulary name cannot be null, empty or whitespace, but was {invalidValue}.";
 
-                name = value;
+                name = value switch {
+                    null => throw new ArgumentException(MakeMessage("null")),
+                    var s when string.IsNullOrEmpty(s) => throw new ArgumentException(MakeMessage("empty")),
+                    var s when string.IsNullOrWhiteSpace(s) => throw new ArgumentException(MakeMessage("whitespace")),
+                    var s => s
+                };
             }
         }
 
-        public Vocabulary(string name)
-        {
+        public Vocabulary(string name) {
             Name = name;
+            CreatedAt = DateTimeOffset.Now;
         }
     }
 }
