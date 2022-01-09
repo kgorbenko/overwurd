09a61af3971005ff9ea4d80c93272089e23cac5c
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2022-01-09 17:43:15 +0300

    Do not catch exceptions on `UserPasswordStore` level

--- src/Overwurd.Web/Services/Auth/Stores/UserPasswordStore.cs
+++ src/Overwurd.Web/Services/Auth/Stores/UserPasswordStore.cs
@@ -4,7 +4,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Logging;
 using Overwurd.Model.Models;
 using Overwurd.Model.Repositories;
 
@@ -13,13 +12,10 @@ namespace Overwurd.Web.Services.Auth.Stores
     public class UserPasswordStore : IUserPasswordStore<User>
     {
         private readonly IOverwurdRepository<User> userRepository;
-        private readonly ILogger<UserPasswordStore> logger;
 
-        public UserPasswordStore([NotNull] IOverwurdRepository<User> userRepository,
-                                 [NotNull] ILogger<UserPasswordStore> logger)
+        public UserPasswordStore([NotNull] IOverwurdRepository<User> userRepository)
         {
             this.userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public void Dispose() { }
@@ -57,44 +53,24 @@ namespace Overwurd.Web.Services.Auth.Stores
 
         public async Task<IdentityResult> CreateAsync(User user, CancellationToken cancellationToken)
         {
-            try
-            {
-                await userRepository.AddAsync(user, cancellationToken);
 
-                return IdentityResult.Success;
-            } catch (Exception exception)
-            {
-                logger.LogError(exception, "");
-                return IdentityResult.Failed();
-            }
+            await userRepository.AddAsync(user, cancellationToken);
+
+            return IdentityResult.Success;
         }
 
         public async Task<IdentityResult> UpdateAsync(User user, CancellationToken cancellationToken)
         {
-            try
-            {
-                await userRepository.UpdateAsync(user, cancellationToken);
+            await userRepository.UpdateAsync(user, cancellationToken);
 
-                return IdentityResult.Success;
-            } catch (Exception exception)
-            {
-                logger.LogError(exception, "");
-                return IdentityResult.Failed();
-            }
+            return IdentityResult.Success;
         }
 
         public async Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken)
         {
-            try
-            {
-                await userRepository.RemoveAsync(user, cancellationToken);
+            await userRepository.RemoveAsync(user, cancellationToken);
 
-                return IdentityResult.Success;
-            } catch (Exception exception)
-            {
-                logger.LogError(exception, "");
-                return IdentityResult.Failed();
-            }
+            return IdentityResult.Success;
         }
 
         public async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken)
