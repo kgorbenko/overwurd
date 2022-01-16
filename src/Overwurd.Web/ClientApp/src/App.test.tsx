c0e4a012bf5c957524a8256009d6a5d7eba34086
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2022-01-16 19:54:06 +0300

    Fix failing test

--- src/Overwurd.Web/ClientApp/src/App.test.tsx
+++ src/Overwurd.Web/ClientApp/src/App.test.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import * as React from 'react';
+import * as dayjs from 'dayjs';
 import ReactDOM from 'react-dom';
+import utc from 'dayjs/plugin/utc';
 import { MemoryRouter } from 'react-router-dom';
 import { App } from './App';
+import { AppContextProvider } from './AppContextProvider';
+import { findByText } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 
 it('renders without crashing', async () => {
+    dayjs.extend(utc);
     const div = document.createElement('div');
     ReactDOM.render(
         <MemoryRouter>
-            <App />
+            <AppContextProvider>
+                <App />
+            </AppContextProvider>
         </MemoryRouter>, div);
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    const hello = await findByText(div, 'Hello, world! Testing new version.');
+    expect(hello).toBeTruthy();
 });
