5681c129ec9da04f54887d6ccfdaee0a099686b1
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2022-01-30 14:18:54 +0300

    Beautify `index.tsx`

--- src/Overwurd.Web/ClientApp/src/App.tsx
+++ src/Overwurd.Web/ClientApp/src/App.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import * as dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
 import { BrowserRouter } from 'react-router-dom';
 import { AppContextProvider } from './AppContextProvider';
 import { Dashboard } from './Dashboard';
 import { theme } from './Theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
-export const App: React.FunctionComponent<{}> = (props: React.PropsWithChildren<{}>) => {
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import './App.css';
+
+dayjs.extend(utc);
+
+export const App: React.FunctionComponent = () => {
     const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') ?? undefined;
     return (
         <BrowserRouter basename={baseUrl}>
--- src/Overwurd.Web/ClientApp/src/index.tsx
+++ src/Overwurd.Web/ClientApp/src/index.tsx
@@ -1,17 +1,6 @@
 import * as React from 'react';
-import * as dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc'
 import { render } from 'react-dom';
 import { App } from './App';
-
-import './Index.css';
-
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
-dayjs.extend(utc);
 
 const rootElement = document.getElementById('root');
 render(<App />, rootElement);
